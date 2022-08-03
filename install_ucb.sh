7b2ba3cc7789a418efefdbecfcc0c989b2407034
John Lowe <jblowe@berkeley.edu>
2022-08-03 10:26:21 -0700

    HMP-243: revisions to install script for bl7

--- install_ucb.sh
+++ install_ucb.sh
@@ -2,7 +2,6 @@
 #set -e
 #set -x
 tenant=$1
-portal_config_file=$2
 current_directory=`pwd`
 extra_dir="../extras"
 
@@ -22,14 +21,14 @@ cd portal || exit
 if [[ ! $WHOLE_LIST =~ .*${tenant}.* || "$tenant" == "" ]]
 then
   echo "1st argument must be one of '${WHOLE_LIST}'"
-  echo "$0 tenant <optional portal config file>"
+  echo "$0 tenant"
   exit
 fi
 
 if [ ! -d "${extra_dir}" ]; then
   echo "Can't find directory '${extra_dir}'. Please verify name and location"
   echo "This script must be executed from the base dir of the ucb blacklight customization (i.e. radiance)"
-  echo "$0 tenant <optional portal config file>"
+  echo "$0 tenant"
   exit
 fi
 
@@ -41,6 +40,7 @@ perl -i -pe "s/#TENANT#/${tenant}/g" ${extra_dir}/* 2>&1
 # nb: the header logos for all ucb tenants are already in the public static directory
 #     we just need to copy the one for this tenant to the right place.
 cp public/header-logo-${tenant}.png public/header-logo.png
+mkdir app/views/shared
 
 # copy a favicon
 # right now we only have PNG favicons...
@@ -51,74 +51,72 @@ cp ${extra_dir}/${tenant}_favicon.png app/assets/images/favicon.png
 # generic helpers and config, but some do need to be configured per-tenant
 cp ${extra_dir}/application.rb config/application.rb
 cp ${extra_dir}/application_helper.rb app/helpers
-cp ${extra_dir}/routes.rb config/routes.rb
-cp ${extra_dir}/catalog_helper_behavior.rb app/helpers/blacklight
+#cp ${extra_dir}/routes.rb config/routes.rb
+####cp ${extra_dir}/catalog_helper_behavior.rb app/helpers/blacklight
 cp ${extra_dir}/blacklight.yml config
 cp ${extra_dir}/blacklight.en.yml config/locales
-
+#
 cp ${extra_dir}/${tenant}_production.rb config/environments/production.rb
 cp ${extra_dir}/${tenant}_blacklight.en.yml config/locales/blacklight.en.yml
-
-# use our generic header, footer, etc. partials
+#
+## use our generic header, footer, etc. partials
 cp ${extra_dir}/_header_navbar.html.erb app/views/shared/
 cp ${extra_dir}/_pdfs.html.erb app/views/shared/
 cp ${extra_dir}/_footer.html.erb app/views/shared/
 cp ${extra_dir}/_splash.html.erb app/views/shared/
-cp ${extra_dir}/_home_text.html.erb app/views/catalog/
-cp ${extra_dir}/_search_form.html.erb app/views/catalog/
-
-# possible customized partials
-#cp ${extra_dir}/_user_util_links.html.erb app/views/
+cp ${extra_dir}/_home_text.html.erb app/views/shared/
+cp ${extra_dir}/_search_form.html.erb app/views/shared/
+#
+## possible customized partials
+##cp ${extra_dir}/_user_util_links.html.erb app/views/
 cp ${extra_dir}/${tenant}_user_util_links.html.erb app/views/_user_util_links.html.erb
 cp ${extra_dir}/${tenant}_catalog_controller.rb app/controllers/catalog_controller.rb
 cp ${extra_dir}/${tenant}_header_navbar.html.erb app/views/shared/_header_navbar.html.erb
 cp ${extra_dir}/${tenant}_footer.html.erb app/views/shared/_footer.html.erb
+
 #cp ${extra_dir}/${tenant}_home_text.html.erb app/views/catalog/_home_text.html.erb
-cp ${extra_dir}/${tenant}_search_form.html.erb app/views/catalog/_search_form.html.erb
-cp ${extra_dir}/${tenant}_show_sidebar.html.erb app/views/catalog/_show_sidebar.html.erb
-cp ${extra_dir}/${tenant}_splash.html.erb app/views/shared/_splash.html.erb
+#cp ${extra_dir}/${tenant}_blacklight.html.erb app/views/layouts/blacklight/base.html.erb
+# splash page goes into _home_text_html.erb for now
+cp ${extra_dir}/${tenant}_splash.html.erb app/views/shared/_home_text.html.erb
+
+cp ${extra_dir}/${tenant}_search_form.html.erb app/views/shared/_search_form.html.erb
+cp ${extra_dir}/${tenant}_show_sidebar.html.erb app/views/shared/_show_sidebar.html.erb
 cp ${extra_dir}/${tenant}_extras.scss app/assets/stylesheets/extras.scss
 cp ${extra_dir}/${tenant}_variables.scss app/assets/stylesheets/_variables.scss
-
-# so far, these two css files are only needed for cinefiles, for tiles on the splash page
-cp ${extra_dir}/${tenant}_tiles.css app/assets/stylesheets/tiles.css
-cp ${extra_dir}/${tenant}_normalize.min.css app/assets/stylesheets/normalize.min.css
-
-# custom signup for cinefiles
-cp ${extra_dir}/${tenant}_new.html.erb app/views/devise/registrations/new.html.erb
-
-# custom cinefiles restricted PDF warning
+#
+## so far, these two css files are only needed for cinefiles, for tiles on the splash page
+#cp ${extra_dir}/${tenant}_tiles.css app/assets/stylesheets/tiles.css
+#cp ${extra_dir}/${tenant}_normalize.min.css app/assets/stylesheets/normalize.min.css
+#
+## custom signup for cinefiles
+#cp ${extra_dir}/${tenant}_new.html.erb app/views/devise/registrations/new.html.erb
+#
+## custom cinefiles restricted PDF warning
 cp ${extra_dir}/${tenant}_pdfs.html.erb app/views/shared/_pdfs.html.erb
-
-# custom cinefiles rendering WARC files
+#
+## custom cinefiles rendering WARC files
 cp ${extra_dir}/${tenant}_warcs.html.erb app/views/shared/_warcs.html.erb
-
-# custom error pages
-if [ -f "${extra_dir}/${tenant}_not_found.html.erb" ] && [ -f "${extra_dir}/${tenant}_not_found.html.erb" ]; then
-  cp ${extra_dir}/${tenant}_errors_controller.rb app/controllers/errors_controller.rb
-  cp ${extra_dir}/${tenant}_errors_helper.rb app/helpers/errors_helper.rb
-  cp ${extra_dir}/${tenant}_not_found.html.erb app/views/errors/not_found.html.erb
-  cp ${extra_dir}/${tenant}_internal_server_error.html.erb app/views/errors/internal_server_error.html.erb
-  cp ${extra_dir}/${tenant}_404.jpg public/404.jpg
-  cp ${extra_dir}/${tenant}_500.jpg public/500.jpg
-  # you have to remove the default error pages otherwise they supercede the custom ones
-  rm public/404.html
-  rm public/500.html
-fi
-
-# other customizations: social, tracking ids, etc.
+#
+## custom error pages
+#if [ -f "${extra_dir}/${tenant}_not_found.html.erb" ] && [ -f "${extra_dir}/${tenant}_not_found.html.erb" ]; then
+#  cp ${extra_dir}/${tenant}_errors_controller.rb app/controllers/errors_controller.rb
+#  cp ${extra_dir}/${tenant}_errors_helper.rb app/helpers/errors_helper.rb
+#  cp ${extra_dir}/${tenant}_not_found.html.erb app/views/errors/not_found.html.erb
+#  cp ${extra_dir}/${tenant}_internal_server_error.html.erb app/views/errors/internal_server_error.html.erb
+#  cp ${extra_dir}/${tenant}_404.jpg public/404.jpg
+#  cp ${extra_dir}/${tenant}_500.jpg public/500.jpg
+#  # you have to remove the default error pages otherwise they supercede the custom ones
+#  rm public/404.html
+#  rm public/500.html
+#fi
+#
+## other customizations: social, tracking ids, etc.
 cp ${extra_dir}/${tenant}_social.html.erb app/views/shared/_social.html.erb
-cp ${extra_dir}/${tenant}_blacklight.html.erb app/views/layouts/blacklight.html.erb
 cp ${extra_dir}/${tenant}_site_image.jpg public/site_image.jpg
-
+#
 # custom global alerts (via global_alerts gem)
 cp ${extra_dir}/${tenant}_global_alerts.rb config/initializers/global_alerts.rb
 mkdir app/views/global_alerts && cp ${extra_dir}/${tenant}_global_alerts.html.erb app/views/global_alerts/_global_alerts.html.erb
-
-# to make a new splash partial for a tenant.
-# e.g. pick out 15 images to include in 4 x 4 splash partial
-# python ${extra_dir}/etc/pick8.py 4
-# python ${extra_dir}/etc/makestatic.py ${tenant}.static.csv > app/views/shared/_splash.html.erb
-
-# reset the extras directory to pristine state
+#
+## reset the extras directory to pristine state
 git checkout -- ${extra_dir}
